2daf94d9b02e8fa0248d79d9719bcf6b404df9c9
Ghassen-bgh <91068824+Ghassen-bgh@users.noreply.github.com>
2022-10-03 11:47:46 +0100

    task 1

---
+++ 0x06-unity-assets_ui/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class TimerTrigger : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        other.gameObject.GetComponent<Timer>().enabled = true;
+    }
+}
