29d0a6f54d12f418d0990a29af9d3f9ac31b1dcc
Ghassen-bgh <91068824+Ghassen-bgh@users.noreply.github.com>
2022-10-17 13:57:16 +0100

    fix

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    private bool Paused;
+    public Canvas PauseCanva;
+
+    public void Pause()
+    {
+        Paused = true;
+        Time.timeScale = 0;
+        PauseCanva.enabled = true;
+    }
+    public void Resume()
+    {
+        Paused = false;
+        Time.timeScale = 1;
+        PauseCanva.enabled = false;
+
+    }
+
+
+    public void Restart()
+    {
+        Scene currentLevel = SceneManager.GetActiveScene();
+        Resume();
+        SceneManager.LoadScene(currentLevel.buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+}
